ffee92f6b29220e39d398d3aa3d05c04982ec4d6
Bakqui <bq1739@gmail.com>
2020-04-21 00:26:49 +0900

    Considering the continuous action policy and reconstruct the structure of AC

--- ActorCritic.py
+++ ActorCritic.py
@@ -8,73 +8,45 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 class Actor:
-    def __init__(self, model, dim_obs, n_action, lr):
+    def __init__(self, model, dim_obs, dim_action, lr):
         self.dim_obs = dim_obs
-        self.n_action = n_action
+        self.dim_action = dim_action
+        self.policy = model
+        self.lr = lr
+
+    def get_action(self, obs):
+        pi = self.policy(obs)
+        pi = pi.detach().numpy()
+        if self.policy.type == 'softmax':
+            return np.random.choice(self.dim_action, p=pi)
+        elif self.policy.type == 'Gaussian':
+            return pi
+
+    def step(self, obs, action):
+        action_target = action
+        if self.policy.type == 'softmax':
+            action_target = [0.] * self.dim_obs
+            action_target[action] = 1
+        action_predict = self.policy(obs)
+
+        return action_target, action_predict
+
+#####################################################################
+# Work to do :  Differentiate the value function approximation
+#               according to the type of the policy of Critic.
+#####################################################################
+class Critic:
+    def __init__(self, model, dim_obs, dim_action, lr):
+        self.dim_obs = dim_obs
+        self.dim_action = dim_action
         self.model = model
         self.lr = lr
 
-    def get_policy(self, obs):
-        logits = torch.zeros(self.n_action)
-        for action in range(self.n_action):
-            logits[action] = torch.matmul(torch.tensor(np.append(obs, action)), self.theta)
-        # logits = logits + self.bias
-
-        return self.policy(logits)
-
-    def get_action(self, obs):
-        pi = self.get_policy(obs)
-        action = np.random.choice(self.n_action, p=pi)
-
-        return action
-
-    def update(self, obs, action, dtheta=None):
-        if dtheta is None:
-            dtheta = self.calculate_dtheta(obs, action)
-        self.theta = 0
-
-    def calculate_mean_feat(self, obs, policy=None):
-        mean_feat = 0
-        if policy is None:
-            policy = self.get_policy(obs)
-
-        for action in range(self.n_action):
-            phi = np.append(obs, action)
-            mean_feat = mean_feat + policy[action]*phi
-
-        return mean_feat
-
-    def calculate_dtheta(self, obs, action, mean_feat=None, policy=None):
-        if mean_feat is None:
-            mean_feat = self.calculate_mean_feat(obs, policy)
-        phi = np.append(obs, action)
-        phi = phi - mean_feat
-
-        return phi
-
-
-class Critic:
-    def __init__(self, dim_obs, n_action, lr):
-        self.dim_obs = dim_obs
-        self.n_action = n_action
-        self.w = torch.tensor(np.random.randn(dim_obs+1), requires_grad=True)
-        self.lr = lr
-
-    def get_values(self, obs, policy):
-        mean_feat = self.calculate_mean_feat(obs, policy)
-        dws = self.calculate_dw(obs, 0, mean_feat)
-        for b in range(1, self.n_action):
-            phi = self.calculate_dw(obs, b, mean_feat)
-            dws = torch.cat([dws, phi], dim=1)
-        q_values = torch.matmul(dws, self.w)
-
-        return q_values
-
     def get_value(self, obs, action, policy):
-        dw = self.calculate_dw(obs, action, policy=policy)
-        q_value = torch.matmul(dw, self.w)
+        values = self.model(obs)
+        value = values[action]
 
-        return q_value
+        return value
 
     def calculate_mean_feat(self, obs, policy=None):
         mean_feat = 0
@@ -102,31 +74,34 @@ class Critic:
 
 
 class AC:
-    def __init__(self, env, actor, critic, gamma, lr_actor, lr_critic,
+    def __init__(self, env, actor, critic, architecture, gamma, lr_actor, lr_critic,
                  criterion_actor, criterion_critic, optimizer_actor, optimizer_critic):
         self.env = env
         self.action = env.action_space
         self.phi_dim = env.observation_space.shape[0]
         self.actor = actor
         self.critic = critic
+        self.architecture = architecture
         self.gamma = gamma
         self.criterion_actor = criterion_actor
         self.criterion_critic = criterion_critic
         self.optimizer_actor = optimizer_actor(actor.parameters(), lr=lr_actor)
         self.optimizer_critic = optimizer_critic(critic.parameters(), lr=lr_critic)
 
-    def update(self, obs, action, reward, next_state):
-        next_q = self.actor.get_values(obs)
-        q_target = reward + self.gamma*torch.max(next_q)
-        q_predict = self.actor.get_value(obs, action)
-        critic_loss = self.criterion_critic(q_target, q_predict)
+    def update(self, obs, action, reward, next_obs):
+        value_target, value_predict, baseline = self.critic.step(obs, action, reward, next_obs, 
+                                                                 self.gamma, self.architecture)
+        action_target, action_predict = self.actor.step(obs, action)
+        critic_loss = self.criterion_critic(value_predict, value_target)
+        actor_loss = self.criterion_actor(action_predict, action_target)*baseline
 
         self.optimizer_critic.zero_grad()
         critic_loss.backward()
         self.optimizer_critic.step()
 
-
-
+        self.optimizer_actor.zero_grad()
+        actor_loss.backward()
+        self.optimizer_actor.step()
 
     def get_action(self, obs):
         action = self.actor.get_action(obs)
