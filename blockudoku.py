b3d881523b7e5672f749124020e70460d9dc8f99
KumjinPark <bq1739@naver.com>
2020-06-29 16:11:51 +0900

    done test

--- blockudoku.py
+++ blockudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 '''
 Blockudoku 환경에 쓰일 블록을 나타내는 object
 
@@ -90,20 +91,25 @@ class Block:
 '''
 Blockudoku 환경
 
-blocks  : 3개의 Block object들을 담아두는 list
-obs     : 현재 보드 상태와 주어진 블록 모양들을 저장해주는 dictionary 
-          보드 상태 <- obs['board']
-          블록 모양들 <- obs['blocks']  
+blocks          : 3개의 Block object들을 담아두는 list
+obs             : 현재 보드 상태와 주어진 블록 모양들을 저장해주는 dictionary 
+                  보드 상태 <- obs['board']
+                  블록 모양들 <- obs['blocks']
+block_remain    : 사용하지 않은 블록 수
+done            : 현재 Terminal state인지 여부
 '''
 class Blockudoku:
     def __init__(self):
         self.blocks = [Block(), Block(), Block()]
         self.obs = None
+        self.block_remain = 0
+        self.done = False
 
     '''
     환경 초기화
     '''
     def reset(self):
+        self.done = False
         obs = dict()
 
         # board
@@ -115,21 +121,35 @@ class Blockudoku:
         board = np.concatenate((left, center, right), axis=1)
         board_array = np.concatenate((top, board, bottom), axis=0)
         obs['board'] = board_array
-
-        # blocks
-        block_arrays = []
-        for i in range(3):
-            block = self.blocks[i]
-            block.sample()
-            block_arrays.append(block.block_array)
-        obs['blocks'] = block_arrays
+        obs['blocks'] = self.init_block()
 
         self.obs = obs
 
         return obs
 
     '''
+    블록 초기화
+    블록을 모두 사용하였을 때 or 환경을 초기화 할 때 사용
+    '''
+    def init_block(self):
+        block_arrays = []
+        for i in range(3):
+            block = self.blocks[i]
+            block.sample()
+            block_arrays.append(block.block_array)
+        self.block_remain = 3
+        return block_arrays
+
+    '''
     transition
+
+    1) 이미 사용된 블록을 선택한 경우, state 변화없이 0의 reward를 줌
+    2) 블록을 불가능한 곳에 놓을 경우, state 변화없이 0의 reward를 줌
+    3) 블록을 채우는데 성공하면, 채운 블록 수 만큼 reward를 줌
+    4) 9칸을 채우면, 블록들이 지워지면서 18의 추가 reward를 주고,
+    5) 한 번에 여러 번 지우면, 각 9만큼 보너스 reward를 줌
+    6) 블록을 모두 사용했으면, 새로 블록들을 받음
+    7) 주어진 블록들을 어느 곳에도 둘 수 없는 경우, terminal state로 판단
     '''
     def step(self, action):
         assert len(action) == 3, \
@@ -143,19 +163,90 @@ class Blockudoku:
             'column number should be in range [2, 10]'
 
         block = self.blocks[k]
-
+        
+        # 이미 사용된 블록 선택
         if block.is_used:
-            return self.obs
+            return self.obs, 0, self.done
 
-        target_board = self.obs['board'][i-2:i+3, j-2:j+3]
-        if np.sum(block.block_array*target_board) > 0:
-            return self.obs
-
+        # 불가능한 곳에 블록을 둠
+        if not self.feasible_act(block.block_array, i, j):
+            return self.obs, 0, self.done
+        
+        # 블록을 두는데 성공
+        reward = np.sum(block.block_array)
         self.obs['board'][i-2:i+3, j-2:j+3] += block.block_array
+
+        #############################################################
+        ########## 블록을 지웠는 지 여부, 보너스 reward 코드 시작 ##########
+        reward = None
+        ########################### 코드 끝 ##########################
+        #############################################################
+
+        # 이미 사용된 블록임을 표시
         block.used()
         self.obs['blocks'][k] = block.block_array
         self.blocks[k] = block
-        return self.obs
+        self.block_remain -= 1
+
+        # 블록을 모두 사용한 경우
+        if self.block_remain == 0:
+            self.obs['blocks'] = self.init_block()
+
+        # terminal 여부
+        done = self.done_test()
+
+        return self.obs, reward, done
+
+    '''
+    블록을 둘 수 있는 곳인지 판단 : convolution layer 연산 활용
+    '''
+    def feasible_act(self, block_array, i, j):
+        target_board = self.obs['board'][i-2:i+3, j-2:j+3]
+        if np.sum(block_array*target_board) == 0:
+            return True
+        else:
+            return False
+
+    '''
+    terminal 여부 판단 : convolution layer 연산 활용
+    (cs231n의 im2col.py assignment 참고함)
+
+    1) 보드 = 이미지, 블록 = 커널
+    2) convolution 연산을 dot product로 한번에 하기 위해
+    3) (5 x 5) 블록을 (25 x 1) column으로 변환
+    4) 마찬가지로 (13 x 13) 보드를 convolution 연산에 맞게 (81 x 25) matrix로 변환
+       (image size = 13, kernel size = 5, stride = 1 -> output size = (13 - 5)/1 + 1 = 9)
+    5) 각 블록마다 convolution을 수행하고
+    6) 0인 값(가능한 action)이 하나라도 있으면 non-terminal
+    '''
+    def done_test(self):
+        done = True
+        board = self.obs['board']
+
+        # 행 번호
+        i0 = np.repeat(np.arange(5), 5)
+        i1 = np.repeat(np.arange(9), 9)
+        i = i0.reshape(1, -1) + i1.reshape(-1, 1)
+
+        # 열 번호
+        j0 = np.tile(np.arange(5), 5)
+        j1 = np.tile(np.arange(9), 9)
+        j = j0.reshape(1, -1) + j1.reshape(-1, 1)
+
+        # convolution 연산에 맞도록 indexing, reshaping
+        board_col = board[i, j]
+
+        # 블록마다 convolution 수행
+        for k in range(3):
+            if self.blocks[k].is_used:
+                continue
+            block_col = self.obs['blocks'][k].reshape(-1, 1)
+            check = np.dot(board_col, block_col)
+            if check.min() == 0:
+                done = False
+                break
+
+        return done
 
 
 # test
@@ -167,7 +258,8 @@ if __name__ == '__main__':
         print('block', k, '\n', obs['blocks'][k], '\n')
 
     # action test
-    next_obs = env.step((0, 2, 2))
+    next_obs, _, _ = env.step((0, 2, 2))
     print('board\n', next_obs['board'], '\n')
     for k in range(3):
         print('block', k, '\n', next_obs['blocks'][k], '\n')
+
